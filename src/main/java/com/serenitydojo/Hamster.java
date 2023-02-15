28514cb95de3146fd24f0ed8b4f4879954b31c5d
Gokhan Mandas <gokhan@mandas.net>
2023-02-15 16:22:12 +0000

    exercise-7 updates

---
+++ src/main/java/com/serenitydojo/Hamster.java
@@ -0,0 +1,15 @@
+package com.serenitydojo;
+
+public class Hamster extends Pet{
+    private final String favoriteToy;
+
+    public Hamster(String name, int age, String favoriteToy) {
+        super(name, age);
+        this.favoriteToy = favoriteToy;
+    }
+
+    public String play() {
+        return "runs in wheel";
+    }
+
+}
